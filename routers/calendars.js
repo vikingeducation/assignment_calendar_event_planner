24a63e5ff10162595bed7624ab8fd9b4ff521a0b
Egle Libby <eglelibby@gmail.com>
2017-04-14 14:09:45 -0400

    start calendars

--- routers/calendars.js
+++ routers/calendars.js
@@ -2,12 +2,28 @@ var express = require("express");
 var router = express.Router();
 var models = require("./../models");
 var User = models.User;
+var Calendar = models.Calendar;
 var sequelize = models.sequelize;
 
+// ----------------------------------------
+// Index
+// ----------------------------------------
 
-
-
-
-
+router.get("/calendars", (req, res) => {
+  let cals;
+  Calendar.findAll()
+    .then(calendars => {
+      cals = calendars;
+      let usersProm = [];
+      cals.forEach(calendar => {
+        usersProm.push(User.findById(calendar.userID));
+      });
+      return Promise.all(usersProm);
+    })
+    .then(response => {
+      console.log(response[0]);
+    })
+    .catch(e => res.status(500).send(e.stack));
+});
 
 module.exports = router;
