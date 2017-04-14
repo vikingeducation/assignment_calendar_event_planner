d487fbcfc233b0b16a5ec44bf97834bcadcfafbe
deven1 <deven@sloan.mit.edu>
2017-04-14 16:35:04 -0400

    resolved error on edit page

--- routers/calendars.js
+++ routers/calendars.js
@@ -1,6 +1,6 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var models = require('./../models');
+var models = require("./../models");
 var Calendar = models.Calendar;
 var sequelize = models.sequelize;
 
@@ -10,7 +10,7 @@ var sequelize = models.sequelize;
 var onIndex = (req, res) => {
   Calendar.findAll()
     .then(calendars => {
-      res.render('calendars/index', { calendars });
+      res.render("calendars/index", { calendars });
     })
     .catch(e => res.status(500).send(e.stack));
 };
@@ -25,23 +25,23 @@ var onIndex = (req, res) => {
 
 // User.findAll({ where: { id: calendars.userId } });
 
-router.get('/', onIndex);
+router.get("/", onIndex);
 
 // ----------------------------------------
 // New
 // ----------------------------------------
-router.get('/new', (req, res) => {
-  res.render('calendars/new');
+router.get("/new", (req, res) => {
+  res.render("calendars/new");
 });
 
 // ----------------------------------------
 // Edit
 // ----------------------------------------
-router.get('/:id/edit', (req, res) => {
+router.get("/:id/edit", (req, res) => {
   Calendar.findById(req.params.id)
     .then(calendar => {
       if (calendar) {
-        res.render('calendars/edit', { calendar });
+        res.render("calendars/edit", { calendar });
       } else {
         res.send(404);
       }
@@ -52,11 +52,11 @@ router.get('/:id/edit', (req, res) => {
 // ----------------------------------------
 // Show
 // ----------------------------------------
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   Calendar.findById(req.params.id)
     .then(calendar => {
       if (calendar) {
-        res.render('calendars/show', { calendar });
+        res.render("calendars/show", { calendar });
       } else {
         res.send(404);
       }
@@ -67,7 +67,7 @@ router.get('/:id', (req, res) => {
 // ----------------------------------------
 // Create
 // ----------------------------------------
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   var body = req.body;
 
   var calendarParams = {
@@ -85,7 +85,7 @@ router.post('/', (req, res) => {
 // ----------------------------------------
 // Update
 // ----------------------------------------
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   var calendarParams = req.body.calendar;
 
   Calendar.update(
@@ -99,8 +99,9 @@ router.put('/:id', (req, res) => {
     }
   )
     .then(() => {
-      req.method = 'GET';
-      res.redirect(`calendars/${req.params.id}`);
+      req.method = "GET";
+      //console.log("inside calendar edit put block ", `${req.params.id}`);
+      res.redirect(`/calendars/${req.params.id}`);
     })
     .catch(e => res.status(500).send(e.stack));
 });
@@ -108,14 +109,14 @@ router.put('/:id', (req, res) => {
 // ----------------------------------------
 // Destroy
 // ----------------------------------------
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   Calendar.destroy({
     where: { id: req.params.id },
     limit: 1
   })
     .then(() => {
-      req.method = 'GET';
-      res.redirect('/calendars');
+      req.method = "GET";
+      res.redirect("/calendars");
     })
     .catch(e => res.status(500).send(e.stack));
 });
--- routers/users.js
+++ routers/users.js
@@ -1,68 +1,63 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var models = require('./../models');
+var models = require("./../models");
 var User = models.User;
 var sequelize = models.sequelize;
-
 
 // ----------------------------------------
 // Index
 // ----------------------------------------
 var onIndex = (req, res) => {
   User.findAll()
-    .then((users) => {
-      res.render('users/index', { users });
+    .then(users => {
+      res.render("users/index", { users });
     })
-    .catch((e) => res.status(500).send(e.stack));
+    .catch(e => res.status(500).send(e.stack));
 };
-router.get('/', onIndex);
-router.get('/users', onIndex);
-
+router.get("/", onIndex);
+router.get("/users", onIndex);
 
 // ----------------------------------------
 // New
 // ----------------------------------------
-router.get('/users/new', (req, res) => {
-  res.render('users/new');
+router.get("/users/new", (req, res) => {
+  res.render("users/new");
 });
-
 
 // ----------------------------------------
 // Edit
 // ----------------------------------------
-router.get('/users/:id/edit', (req, res) => {
+router.get("/users/:id/edit", (req, res) => {
   User.findById(req.params.id)
-    .then((user) => {
+    .then(user => {
       if (user) {
-        res.render('users/edit', { user })
+        res.render("users/edit", { user });
       } else {
         res.send(404);
       }
     })
-    .catch((e) => res.status(500).send(e.stack));
+    .catch(e => res.status(500).send(e.stack));
 });
-
 
 // ----------------------------------------
 // Show
 // ----------------------------------------
-router.get('/users/:id', (req, res) => {
+router.get("/users/:id", (req, res) => {
   User.findById(req.params.id)
-    .then((user) => {
+    .then(user => {
       if (user) {
-        res.render('users/show', { user });
+        res.render("users/show", { user });
       } else {
         res.send(404);
       }
     })
-    .catch((e) => res.status(500).send(e.stack));
+    .catch(e => res.status(500).send(e.stack));
 });
-
 
 // ----------------------------------------
 // Create
 // ----------------------------------------
-router.post('/users', (req, res) => {
+router.post("/users", (req, res) => {
   var body = req.body;
 
   var userParams = {
@@ -73,60 +68,51 @@ router.post('/users', (req, res) => {
   };
 
   User.create(userParams)
-    .then((user) => {
-      res.redirect(`/users/${ user.id }`);
+    .then(user => {
+      res.redirect(`/users/${user.id}`);
     })
-    .catch((e) => res.status(500).send(e.stack));
+    .catch(e => res.status(500).send(e.stack));
 });
-
 
 // ----------------------------------------
 // Update
 // ----------------------------------------
-router.put('/users/:id', (req, res) => {
+router.put("/users/:id", (req, res) => {
   var userParams = req.body.user;
 
-  User.update({
-    fname: userParams.fname,
-    lname: userParams.lname,
-    username: userParams.username,
-    email: userParams.email
-  }, {
-    where: { id: req.params.id },
-    limit: 1
-  })
+  User.update(
+    {
+      fname: userParams.fname,
+      lname: userParams.lname,
+      username: userParams.username,
+      email: userParams.email
+    },
+    {
+      where: { id: req.params.id },
+      limit: 1
+    }
+  )
     .then(() => {
-      req.method = 'GET';
-      res.redirect(`/users/${ req.params.id }`);
+      req.method = "GET";
+      console.log("inside calendar edit put block ", `${req.params.id}`);
+      res.redirect(`/users/${req.params.id}`);
     })
-    .catch((e) => res.status(500).send(e.stack));
+    .catch(e => res.status(500).send(e.stack));
 });
-
 
 // ----------------------------------------
 // Destroy
 // ----------------------------------------
-router.delete('/users/:id', (req, res) => {
+router.delete("/users/:id", (req, res) => {
   User.destroy({
     where: { id: req.params.id },
     limit: 1
   })
     .then(() => {
-      req.method = 'GET';
-      res.redirect('/users');
+      req.method = "GET";
+      res.redirect("/users");
     })
-    .catch((e) => res.status(500).send(e.stack));
+    .catch(e => res.status(500).send(e.stack));
 });
 
-
-
-
 module.exports = router;
-
-
-
-
-
-
-
-
