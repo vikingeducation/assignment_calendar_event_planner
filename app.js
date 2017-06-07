7387f85a5dbb63a3e177445dd2b1fe28aa56f6aa
rabGIT <rabGIT@users.noreply.github.com>
2017-06-07 12:39:42 -0700

    users

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var hbs = require('express-hbs');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -11,6 +12,10 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.engine('hbs', hbs.express4({
+  partialsDir: path.join(__dirname, '/views/shared'),
+  defaultLayout: path.join(__dirname, '/views/layout')
+}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
@@ -18,12 +23,53 @@ app.set('view engine', 'hbs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use((req, res, next) => {
+  var method;
+
+  // Allow method overriding in
+  // the query string and POST data
+  // and remove the key after found
+  if (req.query._method) {
+    method = req.query._method;
+    delete req.query._method;
+  } else if (typeof req.body === 'object' && req.body._method) {
+    method = req.body._method;
+    delete req.body._method;
+  }
+
+  // Upcase the method name
+  // and set the request method
+  // to override it from GET to
+  // the desired method
+  if (method) {
+    method = method.toUpperCase();
+    req.method = method;
+  }
+
+  next();
+});
+
+
+
+var usersRoutes = require('./routes/users');
+app.use('/', usersRoutes);
+
+app.use((req, res, next) => {
+  ['query', 'params', 'body'].forEach((key) => {
+    if (req[key]) {
+      var capKey = key[0].toUpperCase() + key.substr(1);
+      var value = JSON.stringify(req[key], null, 2);
+      console.log(`${ capKey }: ${ value }`);
+    }
+  });
+  next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,4 +89,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+module.exports = app;;
