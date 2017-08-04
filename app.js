6d662392c76072f72c9825d0468e55fe3da13ba8
Alex-Willenbrink <awillenbrink@gmail.com>
2017-08-04 12:35:55 -0500

    main and app

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const expressHandlebars = require("express-handlebars");
+const morgan = require("morgan");
+const methodOverride = require("method-override");
+const getPostSupport = require("express-method-override-get-post-support");
+const morganToolKit = require("morgan-toolkit");
+
+app.use(methodOverride(
+  getPostSupport.callback,
+  getPostSupport.options
+));
+
+// Set up morgan routes
+app.use(morgan('tiny'));
+app.use(morganToolKit);
+
+// Express handlebars
+const hbs = expressHandlebars.create({
+  partials: 'views/',
+  defaultLayout: 'main'
+})
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+const port = process.env.PORT || 3000;
+const host = 'localhost';
+
+let args;
+process.env.NODE_ENV === 'production' ?
+  args = [port] :
+  args = [port, host];
+
+args.push(() => {
+  console.log(`Listening: http://${host}:${port}`);
+});
+
+app.listen.apply(app, args);
