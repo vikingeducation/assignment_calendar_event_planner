82be2206237ba98d14cf08d709decfbce45f8d0f
Greg Filipczak <gregfilipczak@gmail.com>
2017-08-04 16:17:24 +0000

    working on initial setup

---
+++ app.js
@@ -0,0 +1,59 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const methodOverride = require("method-override");
+const getPostSupport = require("express-method-override-get-post-support");
+
+app.use(
+  methodOverride(
+    getPostSupport.callback,
+    getPostSupport.options // { methods: ['POST', 'GET'] }
+  )
+);
+
+app.use(express.static(`${__dirname}/public`));
+
+var morgan = require("morgan");
+app.use(morgan("tiny"));
+app.use((req, res, next) => {
+  ["query", "params", "body"].forEach(key => {
+    if (req[key]) {
+      var capKey = key[0].toUpperCase() + key.substr(1);
+      var value = JSON.stringify(req[key], null, 2);
+      console.log(`${capKey}: ${value}`);
+    }
+  });
+  next();
+});
+
+var usersRoutes = require("./routers/users");
+app.use("/", usersRoutes);
+
+var expressHandlebars = require("express-handlebars");
+
+var hbs = expressHandlebars.create({
+  partialsDir: "views/",
+  defaultLayout: "application"
+});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+// ----------------------------------------
+// Server
+// ----------------------------------------
+var port = process.env.PORT || process.argv[2] || 3000;
+var host = "localhost";
+
+var args;
+process.env.NODE_ENV === "production" ? (args = [port]) : (args = [port, host]);
+
+args.push(() => {
+  console.log(`Listening: http://${host}:${port}`);
+});
+
+app.listen.apply(app, args);
+
+module.exports = app;
