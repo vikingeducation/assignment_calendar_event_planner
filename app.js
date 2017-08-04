f5c49a2a350dcfac63acfdbeb9d44653cb6240d2
Ian Halverson <halversoncode@gmail.com>
2017-08-04 15:43:00 -0400

    built main page

--- app.js
+++ app.js
@@ -5,36 +5,37 @@ const expressHandlebars = require("express-handlebars");
 const morgan = require("morgan");
 const methodOverride = require("method-override");
 const getPostSupport = require("express-method-override-get-post-support");
-const morganToolKit = require("morgan-toolkit");
+const morganToolKit = require("morgan-toolkit")(morgan, {
+  req: ["cookies", "signedCookies"]
+});
 
-app.use(methodOverride(
-  getPostSupport.callback,
-  getPostSupport.options
-));
+app.use(methodOverride(getPostSupport.callback, getPostSupport.options));
 
 // Set up morgan routes
-app.use(morgan('tiny'));
-app.use(morganToolKit);
+app.use(morgan("tiny"));
+app.use(morganToolKit());
 
 // Express handlebars
 const hbs = expressHandlebars.create({
-  partials: 'views/',
-  defaultLayout: 'main'
-})
+  partials: "views/",
+  defaultLayout: "main"
+});
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 const port = process.env.PORT || 3000;
-const host = 'localhost';
+const host = "localhost";
 
 let args;
-process.env.NODE_ENV === 'production' ?
-  args = [port] :
-  args = [port, host];
+process.env.NODE_ENV === "production" ? (args = [port]) : (args = [port, host]);
 
 args.push(() => {
   console.log(`Listening: http://${host}:${port}`);
 });
 
 app.listen.apply(app, args);
+
+app.get("/", (req, res) => {
+  res.render("users");
+});
