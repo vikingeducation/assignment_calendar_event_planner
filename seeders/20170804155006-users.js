a6abc9f03e9be32c73447ff9f1a8aaa22866a0ec
Eric Glover <etgt28@mail.missouri.edu>
2017-08-04 10:56:14 -0500

    creating database

---
+++ seeders/20170804155006-users.js
@@ -0,0 +1,36 @@
+"use strict";
+
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    var users = [];
+    for (let i = 0; i < 30; i++) {
+      users.push({
+        fname: `Foo${i}`,
+        lname: `Bar${i}`,
+        username: `foobar${i}`,
+        email: `foobar${i}@gmail.com`
+      });
+    }
+    return queryInterface.bulkInsert("users", users);
+  },
+
+  down: function(queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
