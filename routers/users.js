d787bc4425bad3a4b904fa09ad86fc1e5a776502
Ian Halverson <halversoncode@gmail.com>
2017-08-04 18:09:24 -0400

    finished users

--- routers/users.js
+++ routers/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 let db = require("../models");
 let Users = db.user;
 
-router.get("/", (req, res) => {
+router.get(["/", "/users"], (req, res) => {
   Users.findAll()
     .then(users => {
       res.render("users", { users });
@@ -13,9 +13,20 @@ router.get("/", (req, res) => {
     });
 });
 
-router.post("/users/:id", (req, res) => {
-  console.log(`${req.body}`);
-  res.end("post request :(");
+router.post("/users/create", (req, res) => {
+  let userInfo = req.body.user;
+  Users.create({
+    firstName: userInfo.firstName,
+    lastName: userInfo.lastName,
+    username: userInfo.username,
+    email: userInfo.email
+  }).then(user => {
+    res.redirect(`/users/${user.id}`);
+  });
+});
+
+router.get("/users/create", (req, res) => {
+  res.render("user_new");
 });
 
 router.get("/users/:id/edit", (req, res) => {
@@ -24,30 +35,35 @@ router.get("/users/:id/edit", (req, res) => {
   });
 });
 
-// router.post("/users/:id/edit/put", (req, res) {
-//
-// })
-//
+router.get("/users/:id", (req, res) => {
+  Users.findById(req.params.id).then(user => {
+    res.render("user_show", { user });
+  });
+});
 
 router.put("/users/:id", (req, res) => {
   let userInfo = req.body.user;
-  console.log(req.body);
+  Users.update(
+    {
+      firstName: userInfo.firstName,
+      lastName: userInfo.lastName,
+      username: userInfo.username,
+      email: userInfo.email
+    },
+    {
+      where: { id: req.params.id }
+    }
+  ).then(() => {
+    req.method = "GET";
+    res.redirect(`/users/${req.params.id}`);
+  });
+});
 
-  console.log(userInfo);
-
-  // Users.update({
-  //   firstName: userInfo.firstName,
-  //   lastName: userInfo.lastName,
-  //   username: userInfo.username,
-  //   email: userInfo.email
-  // }, {
-  //     where: { id: req.params.id }
-  // })
-  // .then(()=> {
-  //   req.method = "GET";
-  //   res.redirect(`/users/${req.params.id}`);
-  // })
-  res.end("hello sir");
+router.delete("/users/:id", (req, res) => {
+  Users.destroy({ where: { id: req.params.id } }).then(() => {
+    req.method = "GET";
+    res.redirect("/");
+  });
 });
 
 module.exports = router;
