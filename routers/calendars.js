ab9c7aa05cf225642db2778d7651bc1e70b828d9
Greg Filipczak <gregfilipczak@gmail.com>
2017-08-04 21:32:46 +0000

    working on calendar routing

--- routers/calendars.js
+++ routers/calendars.js
@@ -2,35 +2,62 @@ var express = require("express");
 var router = express.Router();
 var models = require("./../models");
 var calendars = models.calendars;
+var users = models.users;
 var sequelize = models.sequelize;
 
 let grabCalendars = (req, res) => {
-  calendars.findAll().then(calendar => {
-    res.render("calendars/index", { calendar });
+  calendars.findAll().then(calendars => {
+    res.render("calendars/index", { calendars });
   });
 };
-//grab all the users info
-router.get("/", grabCalendars);
-//router.get("/calendars", grabCalendars);
 
-//create new user
-router.get("/calendars/new", (req, res) => {
+router.get("/", grabCalendars);
+
+router.get("/new", (req, res) => {
   res.render("calendars/new");
 });
 
-router.get("/calendars/:id", (req, res) => {
+router.get("/:id", (req, res) => {
   let id = req.params.id;
-  calendars.findById(id).then(calendar => {
-    res.render("calendars/show", { calendar });
+  calendars.findById(id).then(calendars => {
+    res.render("calendars/show", { calendars });
   });
 });
+//
+// router.get("/calendars/:id/edit", (req, res) => {
+//   let id = req.params.id;
+//   calendars.findById(id).then(calendars => {
+//     res.render("calendars/edit", { calendars });
+//   });
+//   console.log("GOT A A GET /EDIT");
+// });
 
-router.get("/calendars/:id/edit", (req, res) => {
-  let id = req.params.id;
-  calendars.findById(id).then(calendar => {
-    res.render("calendars/edit", { calendar });
-  });
-  console.log("GOT A A GET /EDIT");
+router.post("/", (req, res) => {
+  let params = {
+    name: req.body.name.trim(),
+    email: req.body.email.trim()
+  };
+  users
+    .findAll({
+      where: { email: email }
+    })
+    .then(user => {
+      calendars
+        .create({
+          userid: user.id,
+          name: params.name
+        })
+        .then(calendar => {
+          res.redirect("/");
+        });
+    });
+
+  // users
+  //   .create(params)
+  //   .then(user => {
+  //     res.redirect(`/users/${user.id}`);
+  //   })
+  //   .catch(e => res.status(500).send(e.stack));
 });
 
 module.exports = router;
