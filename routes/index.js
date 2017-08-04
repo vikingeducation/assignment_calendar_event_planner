e8653da0a4e469949e9a696ff7695e30052a0271
Benjamin Soung <soungbenjamin@gmail.com>
2017-08-04 14:36:37 -0400

    refactored to controllers folder

--- routes/index.js
+++ routes/index.js
@@ -1,76 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const { userController } = require("../controllers");
 
-let models = require("./../models");
-let User = models.User;
-
-let onIndex = (req, res) => {
-	User.findAll({ order: ["id"] })
-		.then(users => {
-			res.render("index", { users });
-		})
-		.catch(e => res.status(500).send(e.stack));
-};
-
-let onSingleUser = (req, res) => {
-	let userid = req.params.id;
-
-	User.findById(userid)
-		.then(user => {
-			res.render("user", { user });
-		})
-		.catch(e => res.status(500).send(e.stack));
-};
-
-let onSubmitNewUser = (req, res) => {
-	const { fname, lname, username, email } = req.body;
-
-	const params = { fname, lname, username, email };
-
-	User.create(params)
-		.then(user => {
-			res.redirect(`/users/${user.id}`);
-		})
-		.catch(e => res.status(500).send(e.stack));
-};
-
-let onNewUser = (req, res) => {
+router.get("/user/new", (req, res) => {
 	res.render("create_user");
-};
+});
 
-let onShowEdit = (req, res) => {
-	let userid = req.params.id;
+// Show all users
+router.get(["/", "/users"], userController.showUsers);
 
-	User.findById(userid)
-		.then(user => {
-			res.render("update_user", { user });
-		})
-		.catch(e => res.status(500).send(e.stack));
-};
+// Show single user
+router.get("/users/:id", userController.showUser);
 
-let onUpdateUser = (req, res) => {
-	const { fname, lname, username, email } = req.body;
+// Show edited user
+router.get("/user/:id/edit", userController.showEdited);
 
-	const params = { fname, lname, username, email };
-	const options = { where: { id: req.params.id } };
+// Create new user
+router.post("/user/new", userController.new);
 
-	User.update(params, options)
-		.then(user => {
-			res.redirect(`/users/${req.params.id}`);
-		})
-		.catch(e => res.status(500).send(e.stack));
-};
-
-router.get(["/", "/users"], onIndex);
-
-router.get("/users/:id", onSingleUser);
-
-router.get("/user/new", onNewUser);
-
-router.get("/user/:id/edit", onShowEdit);
-
-router.post("/user/new", onSubmitNewUser);
-
-router.put("/user/:id/edit", onUpdateUser);
+// Edit single user
+router.put("/user/:id/edit", userController.edit);
 
 module.exports = router;
