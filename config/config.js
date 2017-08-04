4b7906e31e1f1ed02540f3b0c13d674be79dc5fd
Benjamin Soung <soungbenjamin@gmail.com>
2017-08-04 12:08:08 -0400

    set up dotenv

---
+++ config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: process.env.USERNAME,
+    password: null,
+    database: "assignment_calendar_event_planner_development",
+    host: "127.0.0.1",
+    dialect: "postgres"
+  },
+  test: {
+    username: process.env.USERNAME,
+    password: null,
+    database: "assignment_calendar_event_planner_test",
+    host: "127.0.0.1",
+    dialect: "postgres"
+  },
+  production: {
+    username: process.env.USERNAME,
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "postgres"
+  }
+};
