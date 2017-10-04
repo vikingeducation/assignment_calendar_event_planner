8bbe5142dbe12d1a9aeb3a55b44dc2f09c450f53
Tyler Ketron <tketron@gmail.com>
2017-10-03 20:03:56 -0700

    created USER model, migrations, and seeds

---
+++ seeders/20171004022610-users.js
@@ -0,0 +1,21 @@
+'use strict';
+var models = require('./../models');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    var users = [];
+    for (let i = 0; i < 10; i++) {
+      users.push({
+        fname: `Foo${i}`,
+        lname: `Bar${i}`,
+        username: `foobar${i}`,
+        email: `foobar${i}@gmail.com`
+      });
+    }
+    return queryInterface.bulkInsert('Users', users);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Users', null, {}, models.User);
+  }
+};
