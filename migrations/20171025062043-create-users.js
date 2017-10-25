3b9498c84b4f8b67a3cf89a2b3ee917469dea3d5
Coelacanth7 <galford151@gmail.com>
2017-10-25 12:54:43 -0700

    seeded

--- migrations/20171025062043-create-users.js
+++ migrations/20171025062043-create-users.js
@@ -1,36 +1,42 @@
-'use strict';
+"use strict";
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      fname: {
-        type: Sequelize.STRING
-      },
-      lname: {
-        type: Sequelize.STRING
-      },
-      username: {
-        type: Sequelize.STRING
-      },
-      email: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Users');
-  }
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.createTable("Users", {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			fname: {
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+			lname: {
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+			username: {
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+			email: {
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: Sequelize.fn("NOW")
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: Sequelize.fn("NOW")
+			}
+		});
+	},
+	down: (queryInterface, Sequelize) => {
+		return queryInterface.dropTable("Users");
+	}
 };
---
+++ seeders/20171025193821-Users.js
@@ -0,0 +1,38 @@
+"use strict";
+var models = require("./../models");
+
+module.exports = {
+	up: (queryInterface, Sequelize) => {
+		/*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+		var users = [];
+		for (let i = 0; i < 10; i++) {
+			users.push({
+				fname: `Foo${i}`,
+				lname: `Bar${i}`,
+				username: `foobar${i}`,
+				email: `foobar${i}@gmail.com`
+			});
+		}
+		return queryInterface.bulkInsert("Users", users);
+	},
+
+	down: (queryInterface, Sequelize) => {
+		/*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+		return queryInterface.bulkDelete("Users", null, {}, models.User);
+	}
+};
