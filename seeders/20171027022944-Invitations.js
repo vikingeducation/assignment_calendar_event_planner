6bf719084608f29b3d782b0e1911b7f50fbdf20a
Coelacanth7 <galford151@gmail.com>
2017-10-26 19:36:00 -0700

    invitation model created

---
+++ seeders/20171027022944-Invitations.js
@@ -0,0 +1,44 @@
+"use strict";
+
+module.exports = {
+	up: (queryInterface, Sequelize) => {
+		/*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+
+		var invitations = [];
+
+		for (let i = 1; i < 5; i++) {
+			invitations.push({
+				event_id: i,
+				user_id: i
+			});
+		}
+
+		return queryInterface.bulkInsert("Invitations", invitations);
+	},
+
+	down: (queryInterface, Sequelize) => {
+		/*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+
+		return queryInterface.bulkDelete(
+			"Invitations",
+			null,
+			{},
+			models.Invitations
+		);
+	}
+};
