ebe8bdd88fa2a42071cf725c61ce57c5392ddfee
maddiereddy <maddiereddy@gmail.com>
2017-11-07 20:46:48 -0800

    initialized with users model

---
+++ app.js
@@ -0,0 +1,93 @@
+var express = require('express');
+var app = express();
+
+
+// ----------------------------------------
+// Body Parser
+// ----------------------------------------
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// ----------------------------------------
+// Method Override
+// ----------------------------------------
+const methodOverride = require('method-override');
+const getPostSupport = require('express-method-override-get-post-support');
+ 
+ 
+app.use(methodOverride(
+  getPostSupport.callback,
+  getPostSupport.options // { methods: ['POST', 'GET'] }
+));
+
+
+// ----------------------------------------
+// Public
+// ----------------------------------------
+app.use(express.static(`${__dirname}/public`));
+
+
+// ----------------------------------------
+// Logging
+// ----------------------------------------
+const morgan = require('morgan');
+const morganToolkit = require('morgan-toolkit')(morgan);
+
+app.use(morganToolkit());
+
+
+// ----------------------------------------
+// Routes
+// ----------------------------------------
+var usersRoutes = require('./routers/users');
+app.use('/', usersRoutes);
+
+
+// ----------------------------------------
+// Template Engine
+// ----------------------------------------
+var expressHandlebars = require('express-handlebars');
+
+var hbs = expressHandlebars.create({
+  partialsDir: 'views/',
+  defaultLayout: 'application'
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+// ----------------------------------------
+// Server
+// ----------------------------------------
+var port = process.env.PORT ||
+  process.argv[2] ||
+  3000;
+var host = 'localhost';
+
+var args;
+process.env.NODE_ENV === 'production' ?
+  args = [port] :
+  args = [port, host];
+
+args.push(() => {
+  console.log(`Listening: http://${ host }:${ port }`);
+});
+
+app.listen.apply(app, args);
+
+
+
+
+module.exports = app;
+
+
+
+
+
+
+
+
+
+
---
+++ seeders/20171108032240-users.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    var users = [];
+    for (let i = 0; i < 5; i++) {
+      users.push({
+        fname: `Foo${ i }`,
+        lname: `Bar${ i }`,
+        username: `foobar${ i }`,
+        email: `foobar${ i }@gmail.com`
+      });
+    }
+    return queryInterface.bulkInsert('Users', users);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
