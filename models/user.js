bd10d6217eaa3b4785402beba9acea83482cc126
Hai Nguyen <memyselfandhai@gmail.com>
2017-12-04 14:43:11 -0600

    need to run seeds

--- models/user.js
+++ models/user.js
@@ -1,16 +1,10 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  var User = sequelize.define('User', {
+  var User = sequelize.define("User", {
     fname: DataTypes.STRING,
     lname: DataTypes.STRING,
     username: DataTypes.STRING,
     email: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
   });
   return User;
 };
