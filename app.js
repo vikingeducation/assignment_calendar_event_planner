deb01459a7871cab67f550d677a0bbf61b640d1e
Hai Nguyen <memyselfandhai@gmail.com>
2017-12-04 18:00:09 -0600

    delete working

--- app.js
+++ app.js
@@ -6,11 +6,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const router = express.Router();
 
 const userRoutes = require("./routers/users");
-app.use("/", userRoutes);
 
 //Method OverRides
 const methodOverride = require("method-override");
 const getPostSupport = require("express-method-override-get-post-support");
+
+// Pass the callback and options from
+// the support package
+app.use(
+  methodOverride(
+    getPostSupport.callback,
+    getPostSupport.options // { methods: ['POST', 'GET'] }
+  )
+);
 
 //Handlebars settings
 app.engine(
@@ -24,18 +32,10 @@ app.set("view engine", "handlebars");
 
 //Morgan settings
 const morgan = require("morgan");
-app.use(morgan("tiny"));
+const morganToolkit = require("morgan-toolkit")(morgan);
+app.use(morganToolkit());
 
-app.use((req, res, next) => {
-  ["query", "params", "body"].forEach(key => {
-    if (req[key]) {
-      var capKey = key[0].toUpperCase() + key.substr(1);
-      var value = JSON.stringify(req[key], null, 2);
-      console.log(`${capKey}: ${value}`);
-    }
-  });
-  next();
-});
+app.use("/", userRoutes);
 
 //Server startup
 app.listen(3000, () => {
--- routers/users.js
+++ routers/users.js
@@ -16,5 +16,56 @@ const onIndex = (req, res) => {
 };
 router.get("/", onIndex);
 router.get("/users", onIndex);
+router.get("/users/:id", (req, res) => {
+  // if (req.query._method === "delete") {
+  //   res.redirect("/users/:id/delete");
+  // }
+  User.findById(req.params.id)
+    .then(user => {
+      if (user) {
+        res.render("user", { user });
+      } else {
+        res.send("404 Not Found.");
+      }
+    })
+    .catch(err => res.status(500).send(err.stack));
+});
+
+router.get("/new", (req, res) => {
+  res.render("new");
+});
+
+router.post("/users", (req, res) => {
+  let body = req.body;
+
+  let userParams = {
+    fname: body.user.fname,
+    lname: body.user.lname,
+    username: body.user.username,
+    email: body.user.email
+  };
+
+  User.create(userParams)
+    .then(user => {
+      res.redirect(`/users/${user.id}`);
+    })
+    .catch(err => {
+      res.status(500).send(err.stack);
+    });
+});
+
+router.delete("/users/:id", (req, res) => {
+  User.destroy({
+    where: { id: req.params.id },
+    limit: 1
+  })
+    .then(() => {
+      req.method = "GET";
+      res.redirect("/users");
+    })
+    .catch(err => {
+      res.status(500).send(err.stack);
+    });
+});
 
 module.exports = router;
